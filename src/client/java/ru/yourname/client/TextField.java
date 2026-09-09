633af4afb4a8cc57dde92f683932c3e61c4c4352
ArtemXtheArtiX <artemshmulev251120071@gmail.com>
2026-09-09 21:15:22 +0700

    Update TextField.java

--- src/client/java/ru/yourname/client/TextField.java
+++ src/client/java/ru/yourname/client/TextField.java
@@ -1,6 +1,6 @@
 package ru.yourname.client;
 
-import net.minecraft.client.gl.RenderPipelines; // ИСПРАВЛЕНО: правильный пакет
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.texture.NativeImage;
@@ -48,10 +48,18 @@ public class TextField {
 	}
 
 	private void loadStatic(InputStream is) throws Exception {
-		NativeImage img = NativeImage.read(is);
-		if (img.getWidth() > MAX_DIMENSION || img.getHeight() > MAX_DIMENSION) img = resizeImage(img, MAX_DIMENSION, MAX_DIMENSION);
+		NativeImage originalImg = NativeImage.read(is);
+		// ИСПРАВЛЕНО: используем final переменную, чтобы она была effectively final для лямбды
+		final NativeImage finalImg;
+		if (originalImg.getWidth() > MAX_DIMENSION || originalImg.getHeight() > MAX_DIMENSION) {
+			finalImg = resizeImage(originalImg, MAX_DIMENSION, MAX_DIMENSION);
+			originalImg.close(); // Освобождаем память оригинала, если он был уменьшен
+		} else {
+			finalImg = originalImg;
+		}
+		
 		MinecraftClient.getInstance().execute(() -> {
-			NativeImageBackedTexture tex = new NativeImageBackedTexture(() -> "customoverlay", img);
+			NativeImageBackedTexture tex = new NativeImageBackedTexture(() -> "customoverlay", finalImg);
 			textureId = Identifier.of("customoverlay", "static_" + imageUrlOrPath.hashCode());
 			MinecraftClient.getInstance().getTextureManager().registerTexture(textureId, tex);
 		});
@@ -63,17 +71,32 @@ public class TextField {
 		reader.setInput(iis);
 		int numFrames = reader.getNumImages(true);
 		MinecraftClient.getInstance().execute(() -> {
-			gifTextureIds = new ArrayList<>(); frameDelays = new ArrayList<>();
+			gifTextureIds = new ArrayList<>(); 
+			frameDelays = new ArrayList<>();
 			for (int i = 0; i < numFrames; i++) {
 				try {
 					java.awt.image.BufferedImage bImg = reader.read(i);
-					NativeImage img = NativeImage.read(new java.io.ByteArrayOutputStream() {{ ImageIO.write(bImg, "png", this); }}.toByteArray());
-					if (img.getWidth() > MAX_DIMENSION || img.getHeight() > MAX_DIMENSION) img = resizeImage(img, MAX_DIMENSION, MAX_DIMENSION);
-					NativeImageBackedTexture tex = new NativeImageBackedTexture(() -> "customoverlay", img);
+					NativeImage originalImg = NativeImage.read(new java.io.ByteArrayOutputStream() {{ 
+						ImageIO.write(bImg, "png", this); 
+					}}.toByteArray());
+					
+					// ИСПРАВЛЕНО: используем final переменную для лямбды
+					final NativeImage finalImg;
+					if (originalImg.getWidth() > MAX_DIMENSION || originalImg.getHeight() > MAX_DIMENSION) {
+						finalImg = resizeImage(originalImg, MAX_DIMENSION, MAX_DIMENSION);
+						originalImg.close();
+					} else {
+						finalImg = originalImg;
+					}
+					
+					NativeImageBackedTexture tex = new NativeImageBackedTexture(() -> "customoverlay", finalImg);
 					Identifier id = Identifier.of("customoverlay", "gif_" + imageUrlOrPath.hashCode() + "_" + i);
 					MinecraftClient.getInstance().getTextureManager().registerTexture(id, tex);
-					gifTextureIds.add(id); frameDelays.add(100);
-				} catch (Exception e) { Customoverlay.LOGGER.warn("Skipped frame " + i); }
+					gifTextureIds.add(id); 
+					frameDelays.add(100);
+				} catch (Exception e) { 
+					Customoverlay.LOGGER.warn("Skipped frame " + i); 
+				}
 			}
 			reader.dispose();
 		});
@@ -89,17 +112,17 @@ public class TextField {
 				resized.setColorArgb(x, y, img.getColorArgb((int)(x / scale), (int)(y / scale)));
 			}
 		}
-		img.close(); return resized;
+		return resized;
 	}
 
 	public void render(DrawContext context, boolean editMode) {
 		if (!isLoaded) return;
 		
-		// ИСПРАВЛЕНО: RenderPipelines.GUI_TEXTURED из правильного пакета
 		if (isGif && gifTextureIds != null && !gifTextureIds.isEmpty()) {
 			long now = System.currentTimeMillis();
 			if (now - lastFrameTime >= (frameDelays.get(currentFrame) / speed)) {
-				currentFrame = (currentFrame + 1) % gifTextureIds.size(); lastFrameTime = now;
+				currentFrame = (currentFrame + 1) % gifTextureIds.size(); 
+				lastFrameTime = now;
 			}
 			Identifier currentId = gifTextureIds.get(currentFrame);
 			context.drawTexture(RenderPipelines.GUI_TEXTURED, currentId, x, y, 0.0f, 0.0f, width, height, width, height);
@@ -113,8 +136,13 @@ public class TextField {
 		}
 	}
 
-	public boolean isMouseOver(double mouseX, double mouseY) { return mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height; }
-	public boolean isOverResizeCorner(double mouseX, double mouseY) { return mouseX >= x + width - 8 && mouseX <= x + width && mouseY >= y + height - 8 && mouseY <= y + height; }
+	public boolean isMouseOver(double mouseX, double mouseY) { 
+		return mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height; 
+	}
+	
+	public boolean isOverResizeCorner(double mouseX, double mouseY) { 
+		return mouseX >= x + width - 8 && mouseX <= x + width && mouseY >= y + height - 8 && mouseY <= y + height; 
+	}
 
 	public void cleanup() {
 		gifTextureIds = null;
