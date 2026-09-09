f2fefbf81d4bf5b5f4599fdab0804f18fc0f96f5
ArtemXtheArtiX <artemshmulev251120071@gmail.com>
2026-09-09 20:39:10 +0700

    Update ConfigManager.java

--- src/client/java/ru/yourname/client/ConfigManager.java
+++ src/client/java/ru/yourname/client/ConfigManager.java
@@ -3,6 +3,7 @@ package ru.yourname.client;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.fabricmc.loader.api.FabricLoader;
+import ru.yourname.Customoverlay;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.file.Path;
