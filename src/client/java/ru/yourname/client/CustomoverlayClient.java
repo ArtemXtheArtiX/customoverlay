e2b228e3ef2bed308a6e014d9579ce25ea376095
ArtemXtheArtiX <artemshmulev251120071@gmail.com>
2026-09-09 20:26:33 +0700

    Update CustomoverlayClient.java

--- src/client/java/ru/yourname/client/CustomoverlayClient.java
+++ src/client/java/ru/yourname/client/CustomoverlayClient.java
@@ -1,10 +1,37 @@
 package ru.yourname.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class CustomoverlayClient implements ClientModInitializer {
+	public static KeyBinding keyEditMode;
+	public static KeyBinding keyAddField;
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		keyEditMode = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			"key.customoverlay.editmode", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "category.customoverlay"
+		));
+		keyAddField = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			"key.customoverlay.addfield", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_K, "category.customoverlay"
+		));
+
+		ConfigManager.load();
+
+		HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
+			OverlayRenderer.render(drawContext);
+		});
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (client.currentScreen == null) {
+				while (keyEditMode.wasPressed()) client.setScreen(new EditOverlayScreen());
+				while (keyAddField.wasPressed()) client.setScreen(new AddFieldScreen());
+			}
+		});
 	}
 }
