f5f7fb4029244b8603a2bcb3070dd570c3bcfc93
ArtemXtheArtiX <artemshmulev251120071@gmail.com>
2026-09-09 20:28:03 +0700

    Create AddFieldScreen.java

---
+++ src/client/java/ru/yourname/client/AddFieldScreen.java
@@ -0,0 +1,46 @@
+package ru.yourname.client;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.text.Text;
+
+public class AddFieldScreen extends Screen {
+	private TextFieldWidget urlField;
+	private boolean isGif = false;
+
+	public AddFieldScreen() { super(Text.literal("Add Overlay Field")); }
+
+	@Override
+	protected void init() {
+		urlField = new TextFieldWidget(this.textRenderer, this.width / 2 - 100, this.height / 2 - 30, 200, 20, Text.literal("URL or File Path"));
+		urlField.setMaxLength(256);
+		this.addDrawableChild(urlField); this.setInitialFocus(urlField);
+
+		this.addDrawableChild(ButtonWidget.builder(Text.literal("Add Image"), btn -> { isGif = false; addField(); }).dimensions(this.width / 2 - 100, this.height / 2 + 10, 95, 20).build());
+		this.addDrawableChild(ButtonWidget.builder(Text.literal("Add GIF"), btn -> { isGif = true; addField(); }).dimensions(this.width / 2 + 5, this.height / 2 + 10, 95, 20).build());
+	}
+
+	private void addField() {
+		String source = urlField.getText().trim();
+		if (!source.isEmpty()) {
+			OverlayRenderer.fields.add(new TextField(50, 50, 128, 128, source, isGif));
+			ConfigManager.save(); close();
+		}
+	}
+
+	@Override
+	public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+		this.renderBackground(context, mouseX, mouseY, delta);
+		super.render(context, mouseX, mouseY, delta);
+		context.drawCenteredTextWithShadow(this.textRenderer, "Enter URL or local file path:", this.width / 2, this.height / 2 - 50, 0xFFFFFF);
+		urlField.render(context, mouseX, mouseY, delta);
+	}
+
+	@Override
+	public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+		if (keyCode == 256) { close(); return true; }
+		return super.keyPressed(keyCode, scanCode, modifiers);
+	}
+}
